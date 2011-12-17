4afb482ee049749e508305aba48cb21abe8df852
NoahY <noahy57@gmail.com>
2011-12-17 15:14:53 +0530

    made much simpler

--- qa-confirm-layer.php
+++ qa-confirm-layer.php
@@ -9,33 +9,17 @@
 			qa_html_theme_base::head_custom();
 			if(in_array($this->template, array('question','ask','not-found')) && qa_opt('confirm_close_plugin')) { 
 				$this->output('<script type="text/javascript">
-jQuery("document").ready(function() {
-	jQuery("textarea").each( function(i) {
+window.onbeforeunload = function(event) {
+	var content = false
+	jQuery("textarea").each( function() {
 		if(this.value) {
-			window.onbeforeunload = function(event) {
-				return "You have entered text; are you sure you wish to leave this page?";
-			}
-			return;
+			content = true;
+			return false;
 		}
-		else 
-			window.onbeforeunload = null;
 	});
-	jQuery("textarea").keyup(function(){checkForContent()});
-	jQuery("form").submit(function(event) {
-		window.onbeforeunload = null;
-	});
-});
-function checkForContent() {
-	jQuery("textarea").each( function(i) {
-		if(this.value) {
-			window.onbeforeunload = function(event) {
-				return "You have entered text; are you sure you wish to leave this page?";
-			}
-			return;
-		}
-		else 
-			window.onbeforeunload = null;
-	});
+	if (content)
+		return "You have entered text; are you sure you wish to leave this page?";
+
 }
 </script>');
 			}
